97f8af165fe04a0be1248317267149c671394a7c
cauã icaro cunha siqueira <142066355+Cauaicaro1709@users.noreply.github.com>
2023-10-05 14:31:06 -0300

    Update Principal.java

--- Lista01/Resolvidas/Q01R/src/br/edu/principal/Principal.java
+++ Lista01/Resolvidas/Q01R/src/br/edu/principal/Principal.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
-		Double n1, n2, n3, n4, soma;
+		double n1, n2, n3, n4, soma;
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Digite sua nota da n1:");
 		n1= sc.nextDouble();
@@ -16,7 +16,7 @@ public class Principal {
 		System.out.print("Digite sua nota da n4:");
 		n4 = sc.nextDouble();
 		soma = n1 + n2 + n3 + n4;
-		System.out.print("Sua media é:"+ soma );
+		System.out.print("Sua media Ã©:"+ soma );
 
 	}
 
