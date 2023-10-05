a6803679023a2c849fadbe04d4d3a3e413ceab2f
cauã icaro cunha siqueira <142066355+Cauaicaro1709@users.noreply.github.com>
2023-10-05 18:28:20 -0300

    Update Principal.java

--- Lista01/Resolvidas/Q05R/src/br/edu/principal/Principal.java
+++ Lista01/Resolvidas/Q05R/src/br/edu/principal/Principal.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
-		Double sal, perc, aumento, novosal;
+		double sal, perc, aumento, novosal;
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Digite seu sal·rio: ");
+		System.out.print("Digite seu sal√°rio: ");
 		sal = sc.nextDouble();
 		System.out.print("Digite seu percentual: ");
 		perc = sc.nextDouble();
 		aumento =  (sal * perc)/100;
 		System.out.println("O valor do seu aumento foi de: "+ aumento);
 		novosal = sal + aumento;
-		System.out.println("Seu novo sal·rio È: "+ novosal);
+		System.out.println("Seu novo sal√°rio √©: "+ novosal);
 
 	}
 
