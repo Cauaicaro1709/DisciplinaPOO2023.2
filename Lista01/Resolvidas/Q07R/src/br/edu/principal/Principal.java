d84f1915128eb11c6983ca7b88fd9ed9a5a82f93
cauã icaro cunha siqueira <142066355+Cauaicaro1709@users.noreply.github.com>
2023-10-05 18:30:11 -0300

    Update Principal.java

--- Lista01/Resolvidas/Q07R/src/br/edu/principal/Principal.java
+++ Lista01/Resolvidas/Q07R/src/br/edu/principal/Principal.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
-		Double sal, salreceber, imp;
+		double sal, salreceber, imp;
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Digite o valor do seu sal·rio: ");
+		System.out.print("Digite o valor do seu sal√°rio: ");
 		sal = sc.nextDouble();
 		imp = sal * 10/100;
 		salreceber = sal + 50 - imp;
-		System.out.print("O valor do seu sal·rio a receber È: "+salreceber);
+		System.out.print("O valor do seu sal√°rio a receber √©: "+salreceber);
 	}
 
 }
